6b9732920da8a7a781f75b1333f3f5f20c07669d
Dev Sathwik P <82132397+Sathwik3010@users.noreply.github.com>
2025-12-11 11:19:27 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+//package com.codegnan.java8;
+//
+//public class Demo implements Interf{
+//
+//	
+//	@Override
+//	public void sum(int a, int b) {
+//		System.out.println(a+b);
+//	}
+//
+//	
+//}
---
+++ MyRunnable.java
@@ -0,0 +1,12 @@
+//package com.codegnan.java8;
+//
+//public class MyRunnable implements Runnable{
+//
+//	@Override
+//	public void run() {
+//		for(int i=0; i<3; i++) {
+//			System.out.println("Child thread");
+//		}
+//	}
+//
+//}
